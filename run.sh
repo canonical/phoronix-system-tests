42dc8093ee4aeaf2dd68dd673ce5c45f968c70ef
Vladimir Petko <vladimir.petko@canonical.com>
2024-06-20 18:12:46 +1200

    update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,7 @@ export WATCHDOG_SENSOR_THRESHOLD=85
 # If it's not returning to lower temps, continue after this many min
 export WATCHDOG_MAXIMUM_WAIT=3
 export NO_FILE_HASH_CHECKS=1
+SCRIPT_DIR=$(dirname "$0")
+PTS_BIN=$SCRIPT_DIR/../pts/phoronix-test-suite
 
 DISPLAY=:0 NO_FILE_HASH_CHECKS=1 PTS_SILENT_MODE=1 $PTS_BIN strict-benchmark alltests
