2872663526212f3230e3a33c120efa90b08730a0
Vladimir Petko <vladimir.petko@canonical.com>
2024-06-20 18:10:17 +1200

    add config

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y git php-cli php-xml 
+
 git submodule init
 git submodule update --recursive
+
 ./pts/phoronix-test-suite list-available-tests
 cp -r alltests ~/.phoronix-test-suite/test-suites/local/
 cp -r test-profiles/* ~/.phoronix-test-suite/test-profiles/
+cp ./config/user-config.xml ~/.phoronix-test-suite/
+
