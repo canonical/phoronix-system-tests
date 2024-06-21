602946dea5157d76b5f4d7ff276d003067ddbae4
Vladimir Petko <vladimir.petko@canonical.com>
2024-06-21 19:39:24 +1200

    remove povray install

--- test-profiles/local/povray-1.2.1/install.sh
+++ test-profiles/local/povray-1.2.1/install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get -y install povray
-
-echo $? > ~/install-exit-status
-
 cd ~
 echo "#!/bin/sh
 echo 1 | /usr/bin/povray -benchmark > \$LOG_FILE 2>&1
