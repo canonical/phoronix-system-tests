44676c85245a994ecc1477ea4c04b16919b0481f
Vladimir Petko <vladimir.petko@canonical.com>
2024-06-26 19:26:32 +1200

    do not override glibc path

--- test-profiles/local/glibc-bench-1.8.0/install.sh
+++ test-profiles/local/glibc-bench-1.8.0/install.sh
@@ -8,7 +8,7 @@ make bench-build
 echo $? > ~/install-exit-status
 cd ~
 echo "#!/bin/sh
-cd glibc-2.39/build-tree/amd64-libc/
-CONV_PATH=\$HOME/glibc-2.39/build-tree/amd64-libc/iconvdata LOCPATH=\$HOME/glibc-2.39/build-tree/amd64-libc/localedata LC_ALL=C   \$HOME/glibc-2.39/build-tree/amd64-libc/elf/ld.so --library-path \$HOME/glibc-2.39/build-tree/amd64-libc/:\$HOME/glibc-2.39/build-tree/amd64-libc/math:\$HOME/glibc-2.39/build-tree/amd64-libc/elf:\$HOME/glibc-2.39/build-tree/amd64-libc/dlfcn:\$HOME/glibc-2.39/build-tree/amd64-libc/nss:\$HOME/glibc-2.39/build-tree/amd64-libc/nis:\$HOME/glibc-2.39/build-tree/amd64-libc/rt:\$HOME/glibc-2.39/build-tree/amd64-libc/resolv:\$HOME/glibc-2.39/build-tree/amd64-libc/mathvec:\$HOME/glibc-2.39/build-tree/amd64-libc/support:\$HOME/glibc-2.39/build-tree/amd64-libc/crypt:\$HOME/glibc-2.39/build-tree/amd64-libc/nptl ./\$@ > \$LOG_FILE 2>&1
+cd glibc-2.39/build-tree/amd64-libc/benchtests
+LC_ALL=C ./\$@ > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > glibc-bench
 chmod +x glibc-bench
