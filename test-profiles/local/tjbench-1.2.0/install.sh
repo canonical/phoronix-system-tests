53e8f898fb0a8645d0555f53d0a34e96341d7116
Vladimir Petko <vladimir.petko@canonical.com>
2024-06-27 09:52:25 +1200

    fix tjbench commnad

--- test-profiles/local/tjbench-1.2.0/install.sh
+++ test-profiles/local/tjbench-1.2.0/install.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 unzip -o jpeg-test-1.zip
-tar -xzvf libjpeg-turbo-2.1.0.tar.gz
-
 echo $? > ~/install-exit-status
 cd ~
 echo "#!/bin/sh
-cd libjpeg-turbo-2.1.0/build
-/usr/bin/tjbench ../../jpeg-test-1.JPG -benchtime 20 -warmup 5 -nowrite > \$LOG_FILE
+/usr/bin/tjbench jpeg-test-1.JPG -benchtime 20 -warmup 5 -nowrite > \$LOG_FILE
 echo \$? > ~/test-exit-status" > tjbench
 chmod +x tjbench
