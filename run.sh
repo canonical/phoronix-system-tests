e8a32549c270fb2e6bb23f174afb87d4cb58abd3
Vladimir Petko <vladimir.petko@canonical.com>
2024-07-03 16:17:46 +1200

    add default result name

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+host=`hostname`
+export TEST_RESULTS_IDENTIFIER=$host-id
+export TEST_RESULTS_NAME=$host--name
+export TEST_RESULTS_DESCRIPTION="${host} result"
+
 # Don't let the device overheat and throttle
 export WATCHDOG_SENSOR=cpu.temp,gpu.temp,sys.temp,hdd.temp
 export WATCHDOG_SENSOR_THRESHOLD=85
@@ -9,4 +14,4 @@ export NO_FILE_HASH_CHECKS=1
 SCRIPT_DIR=$(dirname "$0")
 PTS_BIN=$SCRIPT_DIR/pts/phoronix-test-suite
 
-DISPLAY=:0 NO_FILE_HASH_CHECKS=1 PTS_SILENT_MODE=1 $PTS_BIN strict-benchmark alltests
+DISPLAY=:0 NO_FILE_HASH_CHECKS=1 PTS_SILENT_MODE=1 $PTS_BIN strict-benchmark $1
