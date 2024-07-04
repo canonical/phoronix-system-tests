ac3d935ca13dda03541c670b5c0bb581aa4ec4b2
Vladimir Petko <vladimir.petko@canonical.com>
2024-07-05 11:18:55 +1200

    squash me: syntax errors

--- phoronix-charm/src/openstack_provider.py
+++ phoronix-charm/src/openstack_provider.py
@@ -1,10 +1,31 @@
+"""OpenStack-based test run orchestrator."""
 
-import phoronix_provider
+from phoronix_provider import PhoronixProvider
 
-class OpenStackProvider(phoronix_provider):
+
+class OpenStackProvider(PhoronixProvider):
+    """OpenStack-based test run orchestrator."""
+
     def provision(self, event):
+        """Provision Phoronix workers.
+
+        Args:
+            event (_type_): _description_
+        """
         pass
+
     def remove(self, event):
+        """Remove Phoronix workers.
+
+        Args:
+            event (_type_): _description_
+        """
         pass
+
     def benchmark(self, event):
+        """Run benchmark on Phoronix workers.
+
+        Args:
+            event (_type_): _description_
+        """
         pass
--- phoronix-charm/src/phoronix_provider.py
+++ phoronix-charm/src/phoronix_provider.py
@@ -1,8 +1,29 @@
+"""Test run orchestrator interface."""
+
 
 class PhoronixProvider:
+    """Test run orchestrator interface."""
+
     def provision(self, event):
+        """Provision Phoronix workers.
+
+        Args:
+            event (_type_): _description_
+        """
         pass
+
     def remove(self, event):
+        """Remove Phoronix workers.
+
+        Args:
+            event (_type_): _description_
+        """
         pass
+
     def benchmark(self, event):
+        """Run benchmark on Phoronix workers.
+
+        Args:
+            event (_type_): _description_
+        """
         pass
