4949b1af0bc726024ce18a7f116dfca4eb903ba9
Vladimir Petko <vladimir.petko@canonical.com>
2024-07-05 11:10:19 +1200

    add stubs for the phoronix provider

---
+++ phoronix-charm/src/openstack_provider.py
@@ -0,0 +1,10 @@
+
+import phoronix_provider
+
+class OpenStackProvider(phoronix_provider):
+    def provision(self, event):
+        pass
+    def remove(self, event):
+        pass
+    def benchmark(self, event):
+        pass
---
+++ phoronix-charm/src/phoronix_provider.py
@@ -0,0 +1,8 @@
+
+class PhoronixProvider:
+    def provision(self, event):
+        pass
+    def remove(self, event):
+        pass
+    def benchmark(self, event):
+        pass
