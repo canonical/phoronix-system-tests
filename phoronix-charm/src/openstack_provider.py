5d186e55fa70f11b5e8529a6be545cf8e8021ddc
Vladimir Petko <vladimir.petko@canonical.com>
2024-07-09 11:17:41 +1200

    add configuration to openstack provider

--- phoronix-charm/src/openstack_provider.py
+++ phoronix-charm/src/openstack_provider.py
@@ -1,20 +1,31 @@
 """OpenStack-based test run orchestrator."""
 
-from os import chmod
-from Crypto.PublicKey import RSA
 
-from os import environ
-from os.path import expanduser
 import openstack
 
 from phoronix_provider import PhoronixProvider
+from ssh import PHORONIX_PUBLIC_KEY
+from ssh import PHORONIX_PRIVATE_KEY
+from ssh import SSHConnection
+
+KEYPAIR_NAME = "local"
 
 
 class OpenStackProvider(PhoronixProvider):
     """OpenStack-based test run orchestrator."""
 
     def install(self):
-
+        keypair = self.connection.compute.find_keypair(KEYPAIR_NAME)
+        if not keypair:
+            keypair = self.connection.compute.create_keypair(name=KEYPAIR_NAME)
+            with open(PHORONIX_PUBLIC_KEY, "w") as public_key_file:
+                public_key_file.write(keypair.public_key)
+            with open(PHORONIX_PRIVATE_KEY, "w") as private_key_file:
+                private_key_file.write(keypair.private_key)
+#       try:
+        self.connection.create_security_group_rule(
+            "allow_ssh", protocol="tcp", port_range_min=22, port_range_max=22)
+#       except Err:
 
     def configure(self, config):
         """Configure provider with the charm config.
@@ -23,17 +34,16 @@ class OpenStackProvider(PhoronixProvider):
             config (_type_): _description_
         """
         self.connection = openstack.connect(
-                auth_url=config['auth_url'],
-                project_name=config['project_name'],
-                username=config['username'],
-                password=config['password'],
-                region_name=config['region_name'],
-                user_domain_name=config['user_domain_name'],
-                project_domain_name=config['project_domain_name'],
-                app_name='phoronix',
-                app_version='1.0')
-
-
+            auth_url=config['auth_url'],
+            project_name=config['project_name'],
+            username=config['username'],
+            password=config['password'],
+            region_name=config['region_name'],
+            user_domain_name=config['user_domain_name'],
+            project_domain_name=config['project_domain_name'],
+            app_name='phoronix',
+            app_version='1.0')
+        self._servers = self.connection.list_servers()
 
     def provision(self, event):
         """Provision Phoronix workers.
@@ -41,9 +51,14 @@ class OpenStackProvider(PhoronixProvider):
         Args:
             event (_type_): _description_
         """
-        profile = event.params['profile']
+        name = event.params['profile']
+        image = event.params['image']
         flavor = event.params['flavor']
-        self.connection.
+
+        server = self.connection.create_server(
+            name, image=image, flavor=flavor)
+        self.connection.wait_for_server(server)
+        self._servers.append(server)
         pass
 
     def remove(self, event):
@@ -52,6 +67,8 @@ class OpenStackProvider(PhoronixProvider):
         Args:
             event (_type_): _description_
         """
+        for server in self._servers:
+            self.connection.delete_server(server, force=True)
         pass
 
     def benchmark(self, event):
@@ -60,4 +77,9 @@ class OpenStackProvider(PhoronixProvider):
         Args:
             event (_type_): _description_
         """
+        # connect to servers
+        # submit jobs
+        # wait for completion
+        # return results
+
         pass
