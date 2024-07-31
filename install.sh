73ba8f821f3ec11eb094d883938fb0ed5eedbab5
Vladimir Petko <vladimir.petko@canonical.com>
2024-08-01 08:04:51 +1200

    install.sh: remove commented out code

--- install.sh
+++ install.sh
@@ -4,14 +4,8 @@ for package in `cat dependencies.txt`; do
     DEBIAN_FRONTEND=noninteractive sudo apt-get install -y ${package}
 done
 
-
-#git submodule init
-#git submodule update --recursive
-
 ./pts/phoronix-test-suite list-available-tests
 cp -r alltests ~/.phoronix-test-suite/test-suites/local/
 cp -r test-profiles/* ~/.phoronix-test-suite/test-profiles/
 cp ./config/user-config.xml ~/.phoronix-test-suite/
 
-
-echo !!!add deb-src to the apt sources!!!
