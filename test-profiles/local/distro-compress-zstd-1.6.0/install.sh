ebb1807e0b8c43231cba2694b58d08b9b335afbb
Vladimir Petko <vladimir.petko@canonical.com>
2024-08-16 11:19:46 +1200

    compress profile

--- test-profiles/local/distro-compress-zstd-1.6.0/install.sh
+++ test-profiles/local/distro-compress-zstd-1.6.0/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd ~
-cat > compress-zstd <<EOT
+cat >  distro-compress-zstd <<EOT
 #!/bin/sh
 /usr/bin/zstd -T\$NUM_CPU_CORES \$@ silesia.tar > \$LOG_FILE 2>&1
 sed -i -e "s/\r/\n/g" \$LOG_FILE 
 EOT
-chmod +x compress-zstd
+chmod +x  distro-compress-zstd
