ea4bfa0187626d661b99e71fda0f828f1705fd97
Vladimir Petko <vladimir.petko@canonical.com>
2025-03-14 09:33:55 +1300

    feat: use ubuntu-pts-selection module

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ for package in `cat dependencies.txt`; do
 done
 
 ./pts/phoronix-test-suite list-available-tests
-cp -r alltests ~/.phoronix-test-suite/test-suites/local/
-cp -r test-profiles/* ~/.phoronix-test-suite/test-profiles/
+cp -r ubuntu-pts-selection/*.xml ~/.phoronix-test-suite/test-suites/local/
+cp -r ubuntu-pts-selection/test-profiles/* ~/.phoronix-test-suite/test-profiles/
 cp ./config/user-config.xml ~/.phoronix-test-suite/
 
